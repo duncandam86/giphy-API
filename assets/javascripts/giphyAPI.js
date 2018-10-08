5d6f17d98a5dc6ae3909ad72ed974030d1efcc5c
Duncan Dam <duncandam@dhcp-10-105-49-206.wireless.northwestern.private>
2018-10-08 18:23:29 -0500

    JS update

--- assets/javascripts/giphyAPI.js
+++ assets/javascripts/giphyAPI.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //creating array to store actions 
     //Array already had some actions
     var actionArray = ["jumping", "running", "walking", "catching", "singing", "kissing", "spooning", "lazysleeping"];
-    
+
 
     //function to display the button of the action in the Array
     function displayGiphyButton() {
@@ -20,7 +20,7 @@ $(document).ready(function () {
         }
     }
     //console.log(actionButton);
-    
+
 
     // create new function to add new action into the list
     function addAction() {
@@ -28,7 +28,7 @@ $(document).ready(function () {
             // take the value in the input box and store in new variable
             var newAction = $("#inputAction").val().trim();
             //do not take empty input
-            if (newAction ==! " ") {
+            if (newAction == ! " ") {
                 return;
             }
             //if it's not empty
@@ -41,40 +41,55 @@ $(document).ready(function () {
             }
         })
     }
-   
+
     //create new function for API with an AJAX call
-    function giphyDisplay(){
+    function giphyDisplay() {
         //adding click even to all button
-        
-        $("button").on("click", function(){
+
+        $("button").on("click", function () {
             // create variable to store value of button
             var action = $(this).attr("data-action");
             console.log(action);
             //constructing a queryURL using the action name
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ action +"&api_key=dc6zaTOxFJmzC&limit=10";
-            
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q="
+                + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+
             //performing and AJAX request with the queryURL
             $.ajax({
                 url: queryURL,
                 method: "GET"
             })
-            // After data has been retrieved from the request
-            .then(function(response){
-                console.log (queryURL);
-                console.log (response);
-                // empty div that giphyimages will be shown
-                $("#giphyImages").empty();
-                //storing the data from the AJAX request in the results variable
-                var results = response.data;
+                // After data has been retrieved from the request
+                .then(function (response) {
+                    console.log(queryURL);
+                    console.log(response);
+                    // empty div that giphyimages will be shown
+                    $("#giphyImages").empty();
+                    //storing the data from the AJAX request in the results variable
+                    var results = response.data;
 
-                //looping through each result item
-                for (var i =0; i < results.length; i++){
+                    //looping through each result item
+                    for (var i = 0; i < results.length; i++) {
+                        //creating and storing a div tag
+                        var actionDiv = $("</div>");
 
-                }
-            })
-            
+                        //creating a paragraph tag to store the rating
+                        var pRating = $("<p>").text("Rating:" + results[i].rating);
+
+                        //creating a image tag to store the giphy
+                        var actionGiphyImage = $("<img>");
+                        //setting the src attribute for the giphy image to propery that comes from the result item
+                        actionGiphyImage.attr("src", results[i].images.fixed_height.url);
+
+                        //appending the pRating tag to the div tag
+                        actionDiv.append(pRating);
+                        //appending the image tag to the div tag
+                        actionDiv.append(actionGiphyImage);
+                        // prepend the action div to the html page 
+                        $("#giphyImages").prepend(actionDiv);
+                    }
+                })
         })
-        
 
     }
     //call function
